a262a13db663d1057caeda0676ecae731eacdeb8
liuzhe <liuzhe0223@gmail.com>
2013-03-20 05:12:21 +0800

    fix

--- upload_image.py
+++ upload_image.py
@@ -4,8 +4,11 @@ import urllib2
 import urllib
 import sys
 import json
-
-data = file(sys.argv[1], 'r').read()
+try:
+	data = file(sys.argv[1], 'r').read()
+except:
+	print "please give a picture, and make sure your filename is correct"
+	exit()
 urldata = urllib.urlencode({"image" : data})
 url = "https://api.imgur.com/3/upload"
 r = urllib2.Request(url)
@@ -13,3 +16,6 @@ r.add_data(urldata)
 r.add_header('Authorization','Client-ID 1c49486ec8e9565')
 res = json.loads( urllib2.urlopen(r).read())
 print res['data']['link']
+
+
+#upload a picture to imgur, and return a link
